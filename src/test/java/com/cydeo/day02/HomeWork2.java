24c5ac44e8b9954210c0e415df945ded9ee51280
James Eren <erenjamesjohnson@gmail.com>
2022-06-07 23:29:19 +0100

    day2 HomeWork2.java.class

--- src/test/java/com/cydeo/day02/HomeWork2.java
+++ src/test/java/com/cydeo/day02/HomeWork2.java
@@ -1,11 +1,32 @@
 package com.cydeo.day02;
 
+import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.PracticeCydeoReader;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class HomeWork2 {
+    @Test
+    public void T1() {
+        //1. Open Chrome browser
+        //2. Go to http://zero.webappsecurity.com/login.html
+        Driver.getDriver().get(PracticeCydeoReader.getProperty("BankHeaderVerification"));
+
+        //3. Verify header text
+        //Expected: “Log in to ZeroBank”
+        String expectedHeader = "Log in to ZeroBank";
+        String actualHeader = Driver.getDriver().findElement(By.xpath("//h3")).getText();
+
+        Assert.assertTrue(actualHeader.equalsIgnoreCase(expectedHeader));
+        //Expected:
+
+    }
 }
 /*
 TC #2: Zero Bank header verification
 1. Open Chrome browser
 2. Go to http://zero.webappsecurity.com/login.html
 3. Verify header text
-Expected: “Log in to ZeroBank”
+Expected: “Zero Log in”
  */
