8d27b023266757010e7a9fa39fb9f6eb34dfd82a
James Eren <erenjamesjohnson@gmail.com>
2022-06-07 23:07:39 +0100

    day2 HomeWork1.class

--- src/test/java/com/cydeo/day02/HomeWork1.java
+++ src/test/java/com/cydeo/day02/HomeWork1.java
@@ -1,6 +1,27 @@
 package com.cydeo.day02;
 
+import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.PracticeCydeoReader;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class HomeWork1 {
+    @Test
+    public void T1() {
+
+
+        Driver.getDriver().get(PracticeCydeoReader.getProperty("EtsyTitleVerification"));
+        Driver.getDriver().findElement(By.xpath("//button[@class='wt-btn wt-btn--filled wt-mb-xs-0']")).click();
+
+        //3. Verify title:
+        String expectedTitle = "Etsy United Kingdom - Shop for handmade, vintage, custom, and unique gifts for everyone";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.equalsIgnoreCase(expectedTitle));
+        //Expected:
+
+    }
 }
 
 /*
